ed5c4481f78b1c062b67a3a619959924cfc531f6
Magdalena <magdalena.parks@wearelevels.com>
2024-12-11 10:19:02 +0100

    basic accessibility setup

--- frontend-example-vite/eslint.config.js
+++ frontend-example-vite/eslint.config.js
@@ -1,5 +1,6 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import jsxA11y from 'eslint-plugin-jsx-a11y'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
@@ -14,11 +15,13 @@ export default tseslint.config(
       globals: globals.browser,
     },
     plugins: {
+      'jsx-a11y': jsxA11y,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      ...jsxA11y.flatConfigs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
--- frontend-example-vite/src/App.tsx
+++ frontend-example-vite/src/App.tsx
@@ -1,33 +1,46 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const PAGE_TITLE = 'Example Vite App'
+
 function App() {
   const [count, setCount] = useState(0)
 
+  /**
+   * Accessibility guidelines require that the document title matches
+   * the value in the page's h1. Instead of defining it in two places
+   * (here and index.html) and needing to remember to update it in both
+   * places if we ever change the title, we update it dynamically here.
+   */
+  useEffect(() => {
+    document.title = PAGE_TITLE
+  }, [])
+
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <div>
+          <a href="https://vite.dev" target="_blank">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <h1>{PAGE_TITLE}</h1>
+      </header>
+      <main>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </p>
+        </div>
+      </main>
     </>
   )
 }
